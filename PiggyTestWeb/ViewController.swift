9e3b67867da218c36f17aa120af3fe032611233d
Qingli <qingli527@gmail.com>
2020-09-25 04:26:20 -0700

    Initial Commit

---
+++ PiggyTestWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PiggyTestWeb
+//
+//  Created by Qing Li on 2020/9/25.
+//  Copyright © 2020 Qing Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PiggyTestWebTests/PiggyTestWebTests.swift
@@ -0,0 +1,34 @@
+//
+//  PiggyTestWebTests.swift
+//  PiggyTestWebTests
+//
+//  Created by Qing Li on 2020/9/25.
+//  Copyright © 2020 Qing Li. All rights reserved.
+//
+
+import XCTest
+@testable import PiggyTestWeb
+
+class PiggyTestWebTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
